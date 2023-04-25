ebf7ca0fea32f4c552c462ce6f0d18e5554b68a4
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-04-25 11:33:15 +0200

    add Layout.tsx, update _app.tsx
     add new Layout component and put it into _app.tsx

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,18 @@
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className='h-screen bg-slate-900'>
+      <div className='container h-full mx-auto xl:px-30 max-w-6xl'>
+        <div className='grid grild-cols-4 h-full'>
+          <div className='col-span-3 lg:col-span-2 border-x-[1px] border-neutral-700'>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Layout;
