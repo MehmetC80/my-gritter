7db46f6a1026a5f1ab2b6cb3c2e7502099506836
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-04-26 12:06:44 +0200

    add UserBio.tsx and update:[userId].tsx

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import UserBio from '@/components/users/UserBio';
 import UserHero from '@/components/users/UserHero';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useUser from '@/hooks/useUser';
@@ -23,6 +24,7 @@ const UserView = () => {
     <>
       <Header showBackArrow label={fetchedUser?.name} />
       <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
     </>
   );
 };
